b32677f046257cd85481c453f30d8b06968dcd2d
Erik Alexander Sandvik <erikasan@student.matnat.uio.no>
2021-10-24 16:53:49 +0200

    Update

--- the_oblig/test.py
+++ the_oblig/test.py
@@ -8,3 +8,14 @@ def f(y, theta):
 theta = 1
 
 y = np.linspace(1, 1000, 100000)
+
+def F(theta, n, a):
+    return theta**n*np.exp(-theta*a)
+
+n = 2
+a = 0.01
+theta = np.linspace(0, 10000, 1000000)
+
+sns.set()
+plt.plot(theta, F(theta, n, a))
+plt.show()
